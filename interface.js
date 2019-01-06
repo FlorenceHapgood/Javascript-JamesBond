d5f57d06b7afd091f1fa1c37dc9232580943b82f
Florence Hapgood <flohapgood@icloud.com>
2019-01-06 19:22:12 +0000

    Forgotten couple of tests guiltily added
    Feature tests for wrong answer for question four added

--- interface.js
+++ interface.js
@@ -1,28 +1,23 @@
 $(document).ready(function() {
   var answers = new Answers()
 
-  $('#First_User_Ans').change(function(event) {
-    event.preventDefault();
+  $('#First_User_Ans').change(function() {
     var user_answer = $("#First_User_Ans input[type='radio']:checked").val();
     $('#First_Ans').text(answers.qOne(user_answer))
   })
 
-  $('#Second_User_Ans').change(function(event) {
-    event.preventDefault();
-
+  $('#Second_User_Ans').change(function() {
     var user_answer = $("#Second_User_Ans input[type='radio']:checked").val();
     $('#Second_Ans').text(answers.qTwo(user_answer))
   })
 
 
-  $('#Third_User_Ans').change(function(event){
-    event.preventDefault();
+  $('#Third_User_Ans').change(function(){
     var user_answer = $("#Third_User_Ans input[type='radio']:checked").val()
     $('#Third_Ans').text(answers.qThree(user_answer))
   })
 
-  $('#Fourth_User_Ans').change(function(event){
-    event.preventDefault();
+  $('#Fourth_User_Ans').change(function(){
     var user_answer = $("#Fourth_User_Ans input[type='radio']:checked").val()
     $('#Fourth_Ans').text(answers.qFour(user_answer))
   })
