6c4f2f5021b1734239b24fba0831a4f02ed19926
Florence Hapgood <flohapgood@icloud.com>
2019-01-07 22:05:45 +0000

    eslint on Answers src and spec

--- spec/AnswersSpec.js
+++ spec/AnswersSpec.js
@@ -1,70 +1,72 @@
-describe("Answers", function(){
-  var answers;
-  beforeEach(function(){
+describe('Answers', function() {
+  let answers;
+  beforeEach(function() {
     answers = new Answers();
-  })
+  });
 
-  describe('qOne', function(){
-    it('says if the answer is right', function(){
-      expect(answers.qOne('Dr_No')).toEqual('Ok...well done')
-    })
+  describe('qOne', function() {
+    it('says if the answer is right', function() {
+      expect(answers.qOne('Dr_No')).toEqual('Ok...well done');
+    });
 
-    it('says if the answer is wrong', function(){
-      expect(answers.qOne('Goldfinger_One')).toEqual('WRONG!')
-    })
+    it('says if the answer is wrong', function() {
+      expect(answers.qOne('Goldfinger_One')).toEqual('WRONG!');
+    });
 
-    it('says if the answer is wrong', function(){
-      expect(answers.qOne('From_Russia_With_Love')).toEqual('WRONG!')
-    })
-  })
+    it('says if the answer is wrong', function() {
+      expect(answers.qOne('From_Russia_With_Love')).toEqual('WRONG!');
+    });
+  });
 
-  describe('qTwo', function(){
-    it('says if the answer is right', function(){
-      expect(answers.qTwo('Goldfinger_Two')).toEqual('Hm...not bad, not bad')
-    })
+  describe('qTwo', function() {
+    it('says if the answer is right', function() {
+      expect(answers.qTwo('Goldfinger_Two')).toEqual('Hm...not bad, not bad');
+    });
 
-    it('says if the answer is wrong', function(){
-      expect(answers.qTwo('Diamonds_Are_Forever')).toEqual('You call yourself a James Bond fan?')
-    })
+    it('says if the answer is wrong', function() {
+      expect(answers.qTwo('Diamonds_Are_Forever')).toEqual('You call yourself' +
+      ' a James Bond fan?');
+    });
 
-    it('says if the answer is wrong', function(){
-      expect(answers.qTwo('From_Russia_With_Love')).toEqual('You call yourself a James Bond fan?')
-    })
-  })
+    it('says if the answer is wrong', function() {
+      expect(answers.qTwo('From_Russia_With_Love')).toEqual('You call' +
+      ' yourself a James Bond fan?');
+    });
+  });
 
-  describe('qThree', function(){
-    it('says if the answer is right', function(){
-      expect(answers.qThree('John_Barry')).toEqual('Not bad at all')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qThree('John_Williams')).toEqual('Please...no')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qThree('David_Arnold')).toEqual('Please...no')
-    })
-  })
+  describe('qThree', function() {
+    it('says if the answer is right', function() {
+      expect(answers.qThree('John_Barry')).toEqual('Not bad at all');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qThree('John_Williams')).toEqual('Please...no');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qThree('David_Arnold')).toEqual('Please...no');
+    });
+  });
 
-  describe('qFour', function(){
-    it('says if the answer is right', function(){
-      expect(answers.qFour('Pierce_Brosnan')).toEqual('Impressive')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qFour('Tom_Selleck')).toEqual('Nah')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qFour('Ralph_Fiennes')).toEqual('Nah')
-    })
-  })
+  describe('qFour', function() {
+    it('says if the answer is right', function() {
+      expect(answers.qFour('Pierce_Brosnan')).toEqual('Impressive');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qFour('Tom_Selleck')).toEqual('Nah');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qFour('Ralph_Fiennes')).toEqual('Nah');
+    });
+  });
 
-  describe('qFive', function(){
-    it('says if the answer is right', function(){
-      expect(answers.qFive('Casino_Royale')).toEqual('Nice, very nice')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qFive("On_Her_Majesty's_Secret_Service")).toEqual('Nahuh')
-    })
-    it('says if the answer is wrong', function(){
-      expect(answers.qFive('Live_And_Let_Die')).toEqual('Nahuh')
-    })
-  })
-})
+  describe('qFive', function() {
+    it('says if the answer is right', function() {
+      expect(answers.qFive('Casino_Royale')).toEqual('Nice, very nice');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qFive('On_Her_Majesty\'s_Secret_Service')).toEqual('Nahuh');
+    });
+    it('says if the answer is wrong', function() {
+      expect(answers.qFive('Live_And_Let_Die')).toEqual('Nahuh');
+    });
+  });
+});
--- src/Answers.js
+++ src/Answers.js
@@ -1,43 +1,43 @@
-function Answers(){
+function Answers() {
 
 }
 
-  Answers.prototype.qOne = function(user_answer){
-    if(user_answer === 'Dr_No') {
-      return 'Ok...well done'
-    } else {
-      return 'WRONG!'
-    }
+Answers.prototype.qOne = function(user_answer) {
+  if (user_answer === 'Dr_No') {
+    return 'Ok...well done';
+  } else {
+    return 'WRONG!';
   }
+};
 
-  Answers.prototype.qTwo = function(user_answer){
-    if(user_answer === 'Goldfinger_Two') {
-      return 'Hm...not bad, not bad'
-    } else {
-      return 'You call yourself a James Bond fan?'
-    }
+Answers.prototype.qTwo = function(user_answer) {
+  if (user_answer === 'Goldfinger_Two') {
+    return 'Hm...not bad, not bad';
+  } else {
+    return 'You call yourself a James Bond fan?';
   }
+};
 
-  Answers.prototype.qThree = function(user_answer){
-    if(user_answer === 'John_Barry'){
-      return 'Not bad at all'
-    } else {
-      return 'Please...no'
-    }
+Answers.prototype.qThree = function(user_answer) {
+  if (user_answer === 'John_Barry') {
+    return 'Not bad at all';
+  } else {
+    return 'Please...no';
   }
+};
 
-  Answers.prototype.qFour= function(user_answer){
-    if(user_answer === 'Pierce_Brosnan'){
-      return 'Impressive'
-    } else {
-      return 'Nah'
-    }
+Answers.prototype.qFour= function(user_answer) {
+  if (user_answer === 'Pierce_Brosnan') {
+    return 'Impressive';
+  } else {
+    return 'Nah';
   }
+};
 
-  Answers.prototype.qFive= function(user_answer){
-    if(user_answer === 'Casino_Royale'){
-      return 'Nice, very nice'
-    } else {
-      return 'Nahuh'
-    }
+Answers.prototype.qFive= function(user_answer) {
+  if (user_answer === 'Casino_Royale') {
+    return 'Nice, very nice';
+  } else {
+    return 'Nahuh';
   }
+};
