5425a1f95a66196a0ae287604c06b7a59f43799e
Florence Hapgood <flohapgood@icloud.com>
2019-01-07 23:17:46 +0000

    Eslint for questionremaining feature tests
    And interface file

--- cypress/integration/question_four_spec.js
+++ cypress/integration/question_four_spec.js
@@ -1,24 +1,24 @@
-describe('Question 4', function(){
-  it('asks the question', function(){
-    cy.visit('/')
-    cy.contains('Who was intended to be James Bond instead of Timothy Dalton, but was forced to pull out last minute, due to contract problems with another project?')
-  })
+describe('Question 4', function() {
+  it('asks the question', function() {
+    cy.visit('/');
+    cy.contains('Who was intended to be James Bond instead of Timothy Dalton, but was forced to pull out last minute, due to contract problems with another project?');
+  });
 
-  it('tells you if the answer is right', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('Pierce_Brosnan')
-    cy.contains('Impressive')
-  })
+  it('tells you if the answer is right', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('Pierce_Brosnan');
+    cy.contains('Impressive');
+  });
 
-  it('tells you if the answer is wrong', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('Tom_Selleck')
-    cy.contains('Nah')
-  })
+  it('tells you if the answer is wrong', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('Tom_Selleck');
+    cy.contains('Nah');
+  });
 
-  it('tells you if the answer is wrong', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('Ralph_Fiennes')
-    cy.contains('Nah')
-  })
-})
+  it('tells you if the answer is wrong', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('Ralph_Fiennes');
+    cy.contains('Nah');
+  });
+});
--- interface.js
+++ interface.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-  var answers = new Answers()
+  let answers = new Answers();
 
   $('#First_User_Ans').change(function() {
-    var user_answer = $("#First_User_Ans input[type='radio']:checked").val();
-    $('#First_Ans').text(answers.qOne(user_answer))
-  })
+    let user_answer = $('#First_User_Ans input[type=\'radio\']:checked').val();
+    $('#First_Ans').text(answers.qOne(user_answer));
+  });
 
   $('#Second_User_Ans').change(function() {
-    var user_answer = $("#Second_User_Ans input[type='radio']:checked").val();
-    $('#Second_Ans').text(answers.qTwo(user_answer))
-  })
+    let user_answer = $('#Second_User_Ans input[type=\'radio\']:checked').val();
+    $('#Second_Ans').text(answers.qTwo(user_answer));
+  });
 
 
-  $('#Third_User_Ans').change(function(){
-    var user_answer = $("#Third_User_Ans input[type='radio']:checked").val()
-    $('#Third_Ans').text(answers.qThree(user_answer))
-  })
+  $('#Third_User_Ans').change(function() {
+    let user_answer = $('#Third_User_Ans input[type=\'radio\']:checked').val();
+    $('#Third_Ans').text(answers.qThree(user_answer));
+  });
 
-  $('#Fourth_User_Ans').change(function(){
-    var user_answer = $("#Fourth_User_Ans input[type='radio']:checked").val()
-    $('#Fourth_Ans').text(answers.qFour(user_answer))
-  })
+  $('#Fourth_User_Ans').change(function() {
+    let user_answer = $('#Fourth_User_Ans input[type=\'radio\']:checked').val();
+    $('#Fourth_Ans').text(answers.qFour(user_answer));
+  });
 
-  $('#Fifth_User_Ans').change(function(){
-    var user_answer = $("#Fifth_User_Ans input[type='radio']:checked").val()
-    $('#Fifth_Ans').text(answers.qFive(user_answer))
-  })
+  $('#Fifth_User_Ans').change(function() {
+    let user_answer = $('#Fifth_User_Ans input[type=\'radio\']:checked').val();
+    $('#Fifth_Ans').text(answers.qFive(user_answer));
+  });
 
-})
+});
