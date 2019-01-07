5110b64456cee8c867ab64edd88a0bd6ddc0a536
Florence Hapgood <flohapgood@icloud.com>
2019-01-07 23:13:47 +0000

    Eslint for question3 feature test

--- cypress/integration/question_three_spec.js
+++ cypress/integration/question_three_spec.js
@@ -1,24 +1,24 @@
-describe('Question 3', function(){
-  it('asks the third question', function(){
-    cy.visit('/')
-    cy.contains('Q3) Who wrote the main James Bond theme?')
-  })
+describe('Question 3', function() {
+  it('asks the third question', function() {
+    cy.visit('/');
+    cy.contains('Q3) Who wrote the main James Bond theme?');
+  });
 
-  it('tells you if you were right', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('John_Barry')
-    cy.contains('Not bad at all')
-  })
+  it('tells you if you were right', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('John_Barry');
+    cy.contains('Not bad at all');
+  });
 
-  it('tells you if you were wrong', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('John_Williams')
-    cy.contains('Please...no')
-  })
+  it('tells you if you were wrong', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('John_Williams');
+    cy.contains('Please...no');
+  });
 
-  it('tells you if you were wrong', function(){
-    cy.visit('/')
-    cy.get('[type="radio"]').check('David_Arnold')
-    cy.contains('Please...no')
-  })
-})
+  it('tells you if you were wrong', function() {
+    cy.visit('/');
+    cy.get('[type="radio"]').check('David_Arnold');
+    cy.contains('Please...no');
+  });
+});
